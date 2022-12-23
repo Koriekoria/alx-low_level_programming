f0408d3eba1a5db43eabf303a50c98336f470acc
Koriekoria <jhaildren@gmail.com>
2022-12-23 12:14:18 -0800

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lower case letters to uppercases
+ * @s: string to modify
+ * Return: the results
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+
+			s[i] = s[i] - 32;
+	}
+
+	return (s);
+}
